4e6c6cb56b29e89399a5b52674c68da569b7b572
Freddy Tandazo <fatandaz@espol.edu.ec>
2022-04-11 16:47:47 +0200

    crud UserController - php bin/console make:crud User

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('isVerified')
+            ->add('identification_number')
+            ->add('name')
+            ->add('last_name')
+            ->add('type')
+            ->add('code')
+            ->add('account')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
