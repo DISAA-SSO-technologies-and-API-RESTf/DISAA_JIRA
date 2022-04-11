3d4024a6e8a9c6f66d13b6ee0e6ab2647aec3754
Freddy Tandazo <fatandaz@espol.edu.ec>
2022-04-11 16:32:08 +0200

    Account

---
+++ src/Entity/Account.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $recovery_email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getRecoveryEmail(): ?string
+    {
+        return $this->recovery_email;
+    }
+
+    public function setRecoveryEmail(?string $recovery_email): self
+    {
+        $this->recovery_email = $recovery_email;
+
+        return $this;
+    }
+}
